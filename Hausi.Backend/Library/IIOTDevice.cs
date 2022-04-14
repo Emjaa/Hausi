ac42443654cbb02b856910dadc6c313ba9e77dd3
ole <ole@eggersmann.hamburg>
2022-04-14 17:57:51 +0200

    created relay class

---
+++ Hausi.Backend/Library/IIOTDevice.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+
+public interface IIOTDevice
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+
+    public string IP { get; set; }
+    public string Token { get; set; }
+    
+    void Initialize();
+    void Detach();
+}
---
+++ Hausi.Backend/Library/Implementation/Relays.cs
@@ -0,0 +1,40 @@
+﻿namespace Library;
+
+public class RelaySwitch : IIOTDevice
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public string IP { get; set; }
+    public string Token { get; set; }
+    private ICollection<int> RelayIndices { get; set; }
+
+    public RelaySwitch()
+    {
+        
+    }
+    
+    public void Initialize()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Detach()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TurnOn(IEnumerable<int> indices)
+    {
+          
+    }
+
+    public void TurnOff(IEnumerable<int> indices)
+    {
+        
+    }
+    
+    public void Toggle(IEnumerable<int> indices)
+    {
+        
+    }
+}
